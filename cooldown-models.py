99b5c428d6ea350135e0c27aa0e0ceff6ea9e1a7
Adam Parrott <adam.parrott@ukaea.uk>
2026-03-13 11:12:34 +0000

    Add basic vaguely working example

--- cooldown-models.py
+++ cooldown-models.py
@@ -1,4 +1,4 @@
-from filterpy.kalman import KalmanFilter
+from filterpy.kalman import KalmanFilter, IMMEstimator
 from scipy.signal import cont2discrete
 import pdb
 import numpy as np
@@ -10,6 +10,21 @@ def first_order_sys(K, tau, inputs=None, outputs=None):
 
 dt = 0.1
 t_vec = np.arange(0,10,dt)
+
+def temperature_history():
+    u = np.zeros(t_vec.shape)
+    # window opens in middle for a while
+    u[(4 < t_vec) & (t_vec < 7)] = -1
+
+    sys_c = first_order_sys(1,0.1)
+    yout = ct.forced_response(sys_c, t_vec, inputs=u)
+    # Crudely add the initial level back to simulated output
+    temp = yout.outputs + 5.0
+#    fig, ax, = plt.subplots()
+#    ax.plot(temp)
+    return temp
+
+
 
 class Model:
     def __init__(self, tau, x0=5.0, R=1.0):
@@ -55,35 +70,85 @@ class Estimator(KalmanFilter):
         self.F = model.sys_d.A
         self.x = np.array([[model.x0]])
         self.H = np.array([[1]])
-        self.P *= 1000
         self.R = model.R
         self.Q = .1
+        self.reset() # Set the covariance matrix.
 
-    def run(self):
-        est_state = np.zeros(t_vec.shape)
-        cov = np.zeros(t_vec.shape)
+    def reset(self):
+        self.P = np.identity(self.dim_x)*1000
+
+
+    def run(self, meas):
+        est_state = np.zeros(t_vec.shape+(self.dim_x,1)) # x has dim_x rows and 1 column
+        cov = np.zeros(t_vec.shape+(self.dim_x, self.dim_x)) # cov is x by x
+        log_likelihood = np.zeros(t_vec.shape)
        
-        for i, z in enumerate(model.meas):
+        for i, z in enumerate(meas):
             self.predict()
             self.update(z)
+            log_likelihood[i] = self.log_likelihood
             est_state[i] = self.x
             cov[i] =  self.P
 
-        return est_state, cov
+        self.reset() # Put the covariance back to the initial state, to avoid the 'run' function
+        # having side effects.
+
+        return est_state, cov, log_likelihood
+
+def main():
+   
+    cooling_model = Model(1)
+    estimator = Estimator(cooling_model)
+    
+    stationary_model = Model(1000)
+
+    plot(estimator, cooling_model, "cooling_model")
+    plot(estimator, stationary_model, "stationary_model")
 
 
-model = Model(1)
-estimator = Estimator(model)
-est_state, cov = estimator.run()
+    # IMM setup 
+    R = 0.01
+    temp_hist = temperature_history()
+    temp_data = temp_hist + np.random.normal(0, np.sqrt(R), len(t_vec))
+        
+    filters = [Estimator(cooling_model), Estimator(stationary_model)]
+    
+    M = np.array([[0.5, 0.5],
+                  [0.5, 0.5]]) # This is the matrix that describes model transition likelihood
 
-fig, ax  = plt.subplots(2,1)
-ax[0].set_title("log likelihood = {}".format(estimator.log_likelihood))
-ax[0].plot(t_vec,model.meas)
-ax[0].plot(t_vec, est_state)
-ax[0].fill_between(t_vec, est_state - cov, est_state + cov, alpha=0.2)
+    mu = np.array([5.0,5.0]) # Initital state vector
+    bank = IMMEstimator(filters, mu, M)
+
+    probs = np.zeros(t_vec.shape+(1,2))
+    for i, z in enumerate(temp_data):
+        bank.predict()
+        bank.update(z)
+        probs[i] = bank.mu.copy()
+
+    fig, ax = plt.subplots(2,1)
+    ax[0].plot(t_vec, temp_data)
+    ax[1].plot(t_vec, probs[:,0,0], label="cooling")
+    ax[1].plot(t_vec, probs[:,0,1], label="stationary")
+    ax[0].legend()
 
 
-ax[1].plot(t_vec, cov)
 
 
-plt.show()
+    plt.show()
+
+
+def plot(estimator, model, name=None):
+    est_state, cov, log_likelihood  = estimator.run(model.meas)
+    est_state = est_state[:,0,0]
+    cov = cov[:,0,0]
+    fig, ax  = plt.subplots(2,1)
+    ax[0].set_title("model: {}, log likelihood = {}".format(name, np.sum(log_likelihood)))
+    ax[0].plot(t_vec,model.meas)
+    ax[0].plot(t_vec, est_state)
+    ax[0].fill_between(t_vec, est_state - cov, est_state + cov, alpha=0.2)
+    ax[1].plot(t_vec, log_likelihood)
+
+
+
+if __name__ == "__main__":
+    main()
